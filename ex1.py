276e4288985e0686c42ed1c5bbae83575b3a340d
Elena Zaskalko <zaskalko.lena@gmail.com>
2015-02-04 11:23:55 +0300

    Tone down emotionality

--- ex1.py
+++ ex1.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-print "Hello world!!!"
+print "Hello world!"
 print "Hello again!"
 print "I like typing this"
 print "This is fun"
-print "Yay! Printing!!!"
+print "Yay! Printing!"
 print "I'd much rather you 'not'"
 print 'I "said" not touch this'
